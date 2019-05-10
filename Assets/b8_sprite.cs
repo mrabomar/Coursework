e3cb683f7307c02cba0fc0e27b2d70a9df55d54e
mrabomar <46660985+mrabomar@users.noreply.github.com>
2019-05-10 15:34:56 +0300

    Initial commit

---
+++ Assets/b8_sprite.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class b8_sprite : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float Ycoo;
+
+    private void Current()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Ycoo);
+
+            transform.position = targetPos;
+        }
+
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Ycoo);
+
+            transform.position = targetPos;
+        }
+    }
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
